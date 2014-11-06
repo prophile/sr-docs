c049440f3cbcc239ebba1ad4109249a41a65ddc8
Alistair Lynn <arplynn@gmail.com>
2014-11-06 16:30:14 +0000

    Properly import md and html content-types

--- import-docs.py
+++ import-docs.py
@@ -13,6 +13,8 @@ srweb_root = Path(args.srweb) / 'content' / 'en' / 'docs'
 # Clear out existing content
 for previous_file in docs_root.glob('**/*.md'):
     previous_file.unlink()
+for previous_file in docs_root.glob('**/*.html'):
+    previous_file.unlink()
 
 MD_REGEX = re.compile('^//([A-Z_]+)\s*:\s*(.*)\s*$')
 PRE_START_REGEX = re.compile('^<pre><code class="override-lang ([a-z0-9_-]+)">(.*)')
@@ -37,13 +39,20 @@ def transliterate_markdown(source_f, dest_f):
             else:
                 dest_f.write(line)
 
+def transliterate_html(source_f, dest_f):
+    dest_f.write(source_f.read())
+
+FORMATS = {'markdown': (transliterate_markdown, '.md'),
+           'html': (transliterate_html, '.html'),
+           'md': (transliterate_markdown, '.md')}
+
 def convert_document(source, destination):
     try:
         destination.parent.mkdir(parents=True)
     except FileExistsError:
         pass
     print('Converting', source, '->', destination)
-    with source.open('r') as source_f, destination.open('w') as dest_f:
+    with source.open('r') as source_f:
         metadata = {}
         while True:
             md_line = source_f.readline()
@@ -52,23 +61,23 @@ def convert_document(source, destination):
                 break
             if match.group(2):
                 metadata[match.group(1)] = match.group(2)
-        if metadata.get('CONTENT_TYPE', 'markdown') != 'markdown':
-            print("ERROR: {} is not Markdown".format(source))
-            return
+        content_type = metadata.get('CONTENT_TYPE', 'markdown')
         yaml_data = {'layout': 'default'}
         yaml_data['title'] = metadata.get('TITLE', 'Documentation')
         if 'DESCRIPTION' in metadata:
             yaml_data['description'] = metadata['DESCRIPTION']
-        dest_f.write('---\n')
-        yaml.dump(yaml_data, dest_f, default_flow_style=False)
-        dest_f.write('---\n')
-        transliterate_markdown(source_f, dest_f)
+        transliterator, extension = FORMATS[content_type]
+        with destination.with_suffix(extension).open('w') as dest_f:
+            dest_f.write('---\n')
+            yaml.dump(yaml_data, dest_f, default_flow_style=False)
+            dest_f.write('---\n')
+            transliterator(source_f, dest_f)
 
 for doc_page in srweb_root.glob('**/*'):
     if doc_page.is_dir():
         continue
     if doc_page.name.startswith('.'):
         continue
-    target_path = (docs_root / doc_page.relative_to(srweb_root)).with_suffix('.md')
+    target_path = (docs_root / doc_page.relative_to(srweb_root))
     convert_document(doc_page, target_path)
 
